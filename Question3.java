a7be23885a0622e15ff58296d0cba817cef58fb2
Evremk <80709842+Evremk@users.noreply.github.com>
2021-04-29 22:57:30 +0200

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,26 @@
+package assignment_8;
+
+public class Question3 {
+
+
+	public static void main(String[] args) {
+		System.out.println(hello()+world());
+		
+		
+
+	}
+
+	public static String world() {
+		String str1=" World!";
+		return str1;
+		
+	}
+
+	public static String hello() {
+		String str2="Hello";
+		return str2;
+		
+		
+	}
+
+}
